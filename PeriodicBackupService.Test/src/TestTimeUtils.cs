3e36ad5bf46b2a0cf50772ddd17fe4e516485ab8
ÍIAndreasII <andreas.bartilson@gmail.com>
2021-07-19 23:03:38 +0200

    Add unittests for time utilities

---
+++ PeriodicBackupService.Test/src/TestTimeUtils.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using PeriodicBackupService.Core;
+
+namespace PeriodicBackupService.Test
+{
+	[TestFixture]
+	class TestTimeUtils
+	{
+		[Test]
+		public void TestHoursToMillis()
+		{
+			Assert.That(TimeUtils.HoursToMillis(1).Equals(60 * 60000));
+			Assert.That(TimeUtils.HoursToMillis(1.5).Equals(90 * 60000));
+		}
+
+		[Test]
+		public void TestMinutesToMillis()
+		{
+			Assert.That(TimeUtils.MinutesToMillis(1).Equals(60000));
+			Assert.That(TimeUtils.MinutesToMillis(1.5).Equals(90000));
+		}
+
+		[Test]
+		public void TestMillisToMinutes()
+		{
+			Assert.That(TimeUtils.MillisToMinutes(60000).Equals(1));
+			Assert.That(TimeUtils.MillisToMinutes(90000).Equals(1.5));
+		}
+	}
+}
